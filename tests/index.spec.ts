2785fcadc3363e399e77d5de43c157041a53e8a4
Alejandro Martín de León <alu0101015941@ull.edu.es>
2022-04-21 10:42:14 +0100

    merge from mod

--- tests/index.spec.ts
+++
@@ -1,9 +0,0 @@
-// import 'mocha';
-// import {expect} from 'chai';
-// import {index} from '../src/operation';
-
-// describe('Index Class Tests', () => {
-//   it('', () => {
-//     expect(index()).to.be.eql('prueba');
-//   });
-// });
