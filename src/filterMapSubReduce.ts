942fad5ccad86c3d8896598e586a05682fc04f08
Alejandro Martín de León <alu0101015941@ull.edu.es>
2022-04-21 10:30:42 +0100

    feat: filtermapsubreduce done

---
+++ src/filterMapSubReduce.ts
@@ -0,0 +1,39 @@
+import {Operation} from './operation';
+
+export class FilterMapSubReduce extends Operation {
+  constructor(_list: number[]) {
+    super(_list);
+  }
+
+  reduce(acc: number = 0, curr: number = 0): number {
+    acc = 0;
+    this._list.forEach((value: number) => {
+      curr = value;
+      acc -= curr;
+    });
+    return acc;
+  }
+
+  map(func: (x: number) => number): number[] {
+    let result: number [] = [];
+    this._list.forEach((value: number) => {
+      result.push(func(value));
+    });
+    return result;
+  }
+
+  filter(func: (x:number) => boolean): number[] {
+    let result: number[] = [];
+    this._list.forEach((value: number) => {
+      if (func(value)) {
+        result.push(value);
+      }
+    });
+    return result;
+  }
+}
+
+/*
+la invocación de filter
+filter(lista, (x: number) => {x > 10})
+*/
