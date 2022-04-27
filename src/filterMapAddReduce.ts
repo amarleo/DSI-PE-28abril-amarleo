f4191552fbe3b42925e5063084927390fc25d9e8
Alejandro Martín de León <alu0101015941@ull.edu.es>
2022-04-27 18:37:52 +0100

    coverage test

---
+++ src/filterMapAddReduce.ts
@@ -0,0 +1,51 @@
+import {Operation} from './operation';
+
+/**
+ * FilterMapAddReduce Class
+ */
+export class FilterMapAddReduce extends Operation {
+  constructor(_list: number[]) {
+    super(_list);
+  }
+
+  /**
+   * Reduce method. Given an array, it reduce to an unique number by the add of them
+   * @param acc Accumulable number
+   * @param curr Current number
+   */
+  reduce(acc: number = 0, curr: number = 0): number {
+    acc = 0;
+    this._list.forEach((value: number) => {
+      curr = value;
+      acc += curr;
+    });
+    return acc;
+  }
+
+  /**
+   * Map method. Given a function, it returns a list
+   * @param func function by param
+   */
+  map(func: (x: number) => number): number[] {
+    let result: number [] = [];
+    this._list.forEach((value: number) => {
+      result.push(func(value));
+    });
+    return result;
+  }
+
+
+  /**
+   * Filter method. Given a function, it returns a list
+   * @param func function by param
+   */
+  filter(func: (x:number) => boolean): number[] {
+    let result: number[] = [];
+    this._list.forEach((value: number) => {
+      if (func(value)) {
+        result.push(value);
+      }
+    });
+    return result;
+  }
+}
